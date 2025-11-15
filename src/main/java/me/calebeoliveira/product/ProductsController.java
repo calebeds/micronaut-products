ef82831cf5a9daa45750991c255cb17ac716fa9a
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-11-15 15:47:34 -0300

    add query parameters endpoint

--- src/main/java/me/calebeoliveira/product/ProductsController.java
+++ src/main/java/me/calebeoliveira/product/ProductsController.java
@@ -3,10 +3,13 @@ package me.calebeoliveira.product;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.QueryValue;
 import me.calebeoliveira.InMemoryStore;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller("/products")
 class ProductsController {
@@ -26,4 +29,16 @@ class ProductsController {
     public Product getProduct(@PathVariable Integer id) {
         return store.getProducts().get(id);
     }
+
+    @Get("/filter{?max,offset}")
+    public List<Product> filteredProducts(@QueryValue Optional<Integer> max,
+                                          @QueryValue Optional<Integer> offset) {
+        return store.getProducts()
+                .values()
+                .stream()
+                .skip(offset.orElse(0))
+                .limit(max.orElse(0))
+                .toList();
+    }
+
 }

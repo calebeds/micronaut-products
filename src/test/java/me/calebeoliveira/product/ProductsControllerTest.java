4912521e46f6398f38c0e831a4b0874d9aca1616
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-11-15 14:41:33 -0300

    add test for product list controller

---
+++ src/test/java/me/calebeoliveira/product/ProductsControllerTest.java
@@ -0,0 +1,39 @@
+package me.calebeoliveira.product;
+
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.json.JsonMapper;
+import io.micronaut.json.tree.JsonNode;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@MicronautTest
+class ProductsControllerTest {
+    private static final Logger LOG = LoggerFactory.getLogger(ProductsControllerTest.class);
+
+    @Inject
+    @Client("/products")
+    private HttpClient client;
+
+    @Inject
+    private JsonMapper jsonMapper;
+
+    @Test
+    void shouldReturnTenProducts_whenCallingGetOnProductsEndpoint() throws IOException {
+        JsonNode response = client.toBlocking().retrieve("/", JsonNode.class);
+        LOG.debug("Retrieved products: {}" + logProducts(response));
+        assertEquals(10, response.size());
+    }
+
+    private String logProducts(JsonNode response) throws IOException {
+        return jsonMapper.writeValueAsString(response);
+    }
+}

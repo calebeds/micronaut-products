8c0dbc387c87dca223c4f01f0c374cc43aeb0979
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-11-16 22:22:20 -0300

    add custom status code for post product

--- src/main/java/me/calebeoliveira/admin/product/AdminProductsController.java
+++ src/main/java/me/calebeoliveira/admin/product/AdminProductsController.java
@@ -1,9 +1,12 @@
 package me.calebeoliveira.admin.product;
 
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Status;
+import io.micronaut.http.exceptions.HttpStatusException;
 import me.calebeoliveira.InMemoryStore;
 import me.calebeoliveira.product.Product;
 
@@ -16,9 +19,14 @@ class AdminProductsController {
         this.store = store;
     }
 
+    @Status(HttpStatus.CREATED)
     @Post(consumes = MediaType.APPLICATION_JSON,
             produces = MediaType.APPLICATION_JSON)
     public Product addNewProduct(@Body Product product) {
+        if(store.getProducts().containsKey(product.id())) {
+            throw new HttpStatusException(HttpStatus.CONFLICT,
+                    "Product with id " + product.id() + " already exists");
+        }
         return store.addProduct(product);
     }
 }
